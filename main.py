bc705612a0236e7cc10f8fecbc1d2da5aea4afb9
dip-mandal <167052758+dip-mandal@users.noreply.github.com>
2026-02-07 02:31:50 +0530

    Change CORS settings to allow all origins
    Updated CORS configuration to allow all origins.

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ app = FastAPI(
 # Replace "*" with specific domains (e.g., ["http://localhost:3000"]) in production
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://dip-mandal.github.io/meixup_frontend"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
